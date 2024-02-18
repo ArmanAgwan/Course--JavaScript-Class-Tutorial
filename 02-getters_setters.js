c9b18123f5ae8418d6b5fb6bd9a027a38028ba5b
ArmanAgwan <armanagwa@gmail.com>
2024-02-18 09:38:50 +0530

    Get&Set

---
+++ 02-getters_setters.js
@@ -0,0 +1,27 @@
+class Square {
+    constructor (_width) {
+        this._width = _width;
+        this._width = _width;
+        this.numOfRequestsForArea = 0;
+    }
+
+    get area() {
+        this.numOfRequestsForArea++;
+        return this.width * this.height;
+    }
+
+    set area(area) {
+        this.width = Math.sqrt(area);
+        this.height = this.width;
+    }
+}
+
+let square1 = new Square(4);
+square1.area = 25;
+
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.area);
+
+console.log(square1.numOfRequestsForArea);
